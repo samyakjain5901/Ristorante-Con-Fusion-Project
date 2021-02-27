b46d36b135594509240586b3dfe48e43a6c1bf48
Samyak Jain <samyak.jain5901@gmail.com>
2021-02-27 18:50:58 +0530

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const minLength = (len) => (val) => (val) && (val.length >= len);
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -101,13 +102,18 @@ function RenderDish({dish}) {
 	if(dish != null){
         return(
         	<div className="col-12 col-md-5 m-1">
-            	<Card>
-            	    <CardImg top src={baseUrl + dish.image} alt={dish.name}/>
-            	    <CardBody>
-            	        <CardTitle>{dish.name}</CardTitle>
-            	        <CardText>{dish.description}</CardText>
-            	    </CardBody>
-            	</Card>
+        	    <FadeTransform in 
+	                transformProps={{
+	                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+	            	<Card>
+	            	    <CardImg top src={baseUrl + dish.image} alt={dish.name}/>
+	            	    <CardBody>
+	            	        <CardTitle>{dish.name}</CardTitle>
+	            	        <CardText>{dish.description}</CardText>
+	            	    </CardBody>
+	            	</Card>
+	            </FadeTransform>
             </div>
         );
 	}
@@ -122,21 +128,25 @@ function RenderComments({comments, postComment, dishId}) {
 	if(comments != null) {
         const cmt = comments.map((comment) => {
 			return (
-                <div key={comment.id}>
+			    <Fade in>
+                    <li key={comment.id}>
                     <p>{comment.comment}</p>
                     <p>--{comment.author} , {new Intl.DateTimeFormat('en-US', {
                         year: 'numeric',
                         month: 'short',
                         day: '2-digit'
                     }).format(new Date(Date.parse(comment.date)))}</p>
-                </div>
+                    </li>
+                </Fade>
 			);
 	    });
 		return (
             <div className="col-12 col-md-5 m-1">
 			    <h4>Comments</h4>
 			    <ul className="list-unstyled">
-			      {cmt}			      
+			        <Stagger in>
+			            {cmt}
+			        </Stagger>			      
 			    </ul>
 			    <CommentForm dishId={dishId} postComment={postComment} />
 			</div>
